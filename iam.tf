e3c7931f94eb63e9e9048a87747d9b5c9966b996
Eric Dahl <eric.c.dahl@gmail.com>
2018-05-25 15:23:59 -0700

    initial

---
+++ iam.tf
@@ -0,0 +1,103 @@
+
+
+resource "aws_iam_role" "ssm" {
+  name = "ssm"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "ssm-instance-profile"
+  role = "${aws_iam_role.ssm.name}"
+}
+
+
+# AmazonEC2RoleforSSM minus S3
+resource "aws_iam_role_policy" "ssm" {
+  role = "${aws_iam_role.ssm.id}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:PutMetricData"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstanceStatus"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ds:CreateComputer",
+                "ds:DescribeDirectories"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source        = "github.com/ericdahl/tf-vpc"
+  admin_ip_cidr = "${var.admin_cidr}"
+}
+
+
+resource "aws_launch_configuration" "default" {
+  image_id      = "ami-14c5486b"
+  instance_type = "t2.medium"
+  key_name      = "ec2-temp-2017-04-21"
+  spot_price    = "0.046400"
+
+  iam_instance_profile = "${aws_iam_instance_profile.ssm.name}"
+
+  security_groups = [
+    "${module.vpc.sg_allow_egress}",
+    "${module.vpc.sg_allow_22}",
+    "${module.vpc.sg_allow_vpc}",
+  ]
+}
+
+resource "aws_autoscaling_group" "default" {
+  launch_configuration = "${aws_launch_configuration.default.name}"
+  max_size = 5
+  min_size = 5
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+
+  vpc_zone_identifier = [
+    "${module.vpc.subnet_public1}",
+    "${module.vpc.subnet_public2}",
+    "${module.vpc.subnet_public3}",
+  ]
+
+  tag {
+    key = "Name"
+    propagate_at_launch = false
+    value = "hello-ssm"
+  }
+}
+
+
+resource "aws_ssm_document" "foo" {
+  name          = "test_document"
+  document_type = "Command"
+
+  content = <<DOC
+  {
+    "schemaVersion": "1.2",
+    "description": "Check ip configuration of a Linux instance.",
+    "parameters": {
+
+    },
+    "runtimeConfig": {
+      "aws:runShellScript": {
+        "properties": [
+          {
+            "id": "0.aws:runShellScript",
+            "runCommand": ["ifconfig"]
+          }
+        ]
+      }
+    }
+  }
+DOC
+}
+
